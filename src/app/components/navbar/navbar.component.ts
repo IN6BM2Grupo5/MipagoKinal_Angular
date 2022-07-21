1959dd5892e2f8bd8d0dd4a9886654a82319ef95
Gabriel Hurtarte <ghurtarte-2020020@kinal.edu.gt>
2022-07-20 20:17:14 -0600

    implementación node

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  providers: [UsuariosService]
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public identidad;
+  public usuario;
+
+  constructor( public _UsuariosService: UsuariosService) {
+    this.identidad = this._UsuariosService.obtenerIdentidad();
+  }
 
   ngOnInit(): void {
+    this.usuario = this._UsuariosService.obtenerIdentidad().usuario;
   }
+
+  logOut(){
+    localStorage.clear();
+  }
+
 
 }
