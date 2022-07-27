3c8b246155b19d428b6bfc0027d9a0a7f290678e
Gabriel Hurtarte <ghurtarte-2020020@kinal.edu.gt>
2022-07-27 10:54:24 -0600

    implementación node

--- src/app/components/usuario-cafeteria/usuario-cafeteria.component.ts
+++ src/app/components/usuario-cafeteria/usuario-cafeteria.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+import { ProductosService } from 'src/app/services/productos.service';
+import { productos } from 'src/app/models/productos.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-usuario-cafeteria',
   templateUrl: './usuario-cafeteria.component.html',
-  styleUrls: ['./usuario-cafeteria.component.scss']
+  styleUrls: ['./usuario-cafeteria.component.scss'],
+  providers: [UsuariosService, ProductosService]
 })
 export class UsuarioCafeteriaComponent implements OnInit {
-
-  constructor() { }
+  public identidad;ss
+  public ProductosModelGet: productos;
+  constructor(private _UsuariosService: UsuariosService, private _ProductosService: ProductosService) {
+    this.identidad = this._UsuariosService.obtenerIdentidad();
+  }
 
   ngOnInit(): void {
+    this.getProductos()
   }
+
+  getProductos(){
+    this._ProductosService.obtenerProductosCafeteria(this._UsuariosService.obtenerToken()).subscribe(
+      (response) => {
+          this.ProductosModelGet = response.productos;
+
+          console.log(response.productos)
+      },
+      (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.error.mensaje
+        })
+      }
+    )
+  }
+
+
 
 }
