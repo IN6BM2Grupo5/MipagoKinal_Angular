fbc9ed0ab814ea84c0ac371d615848dd97124811
Gabriel Hurtarte <ghurtarte-2020020@kinal.edu.gt>
2022-07-30 03:46:17 -0600

    implementación node

--- src/app/components/documento-marbete/documento-marbete.component.ts
+++ src/app/components/documento-marbete/documento-marbete.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-documento-marbete',
@@ -6,12 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./documento-marbete.component.scss']
 })
 export class DocumentoMarbeteComponent implements OnInit {
-
-  constructor() { }
+  public identidad;
+  constructor(private _UsuariosService: UsuariosService) {
+   }
 
   ngOnInit(): void {
+    this.getIdentidad()
   }
 
+  getIdentidad() {
+    this._UsuariosService.obtenerAlumnoId(this._UsuariosService.obtenerIdentidad()._id, this._UsuariosService.obtenerToken()).subscribe(
+      (response) => {
+          this.identidad = response.usuario;
+          this.identidad.correo = this.identidad.correo.replace("@kinal.edu.gt", "")
+          console.log(response.usuario)
+      },
+      (error) => {
+      }
+    )
+  }
     printPage() {
     window.print()
   }
